52d7aa24e16ea523654eac854bc6a7cb1e498e21
SlavkoDudka <114617260+SlavkoDudka@users.noreply.github.com>
2023-05-15 13:18:04 +0300

    Add files via upload

---
+++ lab-1.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        double r = 3;
+        double h = 10;
+
+        double lateralArea = 2 * Math.PI * r * h;
+
+        double volume = Math.PI * r * r * h;
+
+        Console.WriteLine("Площа бічної поверхні циліндра: " + lateralArea);
+        Console.WriteLine("Об'єм циліндра: " + volume);
+    }
+}
