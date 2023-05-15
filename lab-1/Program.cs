0df9328947903d51a342497ce845ee0fb1628be2
SlavkoDudka <114617260+SlavkoDudka@users.noreply.github.com>
2023-05-15 13:31:44 +0300

    Add files via upload

---
+++ lab-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double r, h, s, v;
+
+        Console.Write("Введіть радіус циліндра: ");
+        r = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть висоту циліндра: ");
+        h = Convert.ToDouble(Console.ReadLine());
+
+        s = 2 * Math.PI * r * h;
+
+        v = Math.PI * r * r * h;
+
+        Console.WriteLine("Площа бічної поверхні циліндра: " + s);
+        Console.WriteLine("Об'єм циліндра: " + v);
+    }
+}
